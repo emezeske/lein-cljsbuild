1ef8c1b1ec1eb58f079ca0d297ad3dbd30fe965d
Evan Mezeske <evan@meebo-inc.com>
2011-12-29 15:25:47 -0700

    Adding a :url to point to the upstream code.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject emezeske/lein-cljsbuild "0.0.1"
   :description "ClojureScript Autobuilder Plugin"
+  :url "http://github.com/emezeske/lein-cljsbuild"
   :dependencies [[emezeske/cljsbuild "0.0.1"]]
   :eval-in-leiningen true)
