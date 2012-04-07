a596c5a022740df63786f78dc0df9384090d9ec1
Evan Mezeske <evan@mezeske.com>
2012-04-07 14:30:53 -0700

    Add a couple utility scripts that I use.

---
+++ bin/release.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Release a specific branch to master.
+
+set -e
+
+branch=$1
+if [ -z "$1" ]; then
+    echo "Usage: $0 <branch>."
+    exit 1
+fi
+
+read -p "Are you sure you want to release $branch to master (y/n)? " -n 1 reply
+echo
+if [[ ! $reply =~ ^[Yy]$ ]]; then
+    echo 'Aborting.'
+    exit 1
+fi
+
+echo "Releasing $branch."
+git checkout master
+git merge $branch
+git push --all
+git checkout $branch
+git tag -m "Release ${branch}." $branch
+git push --tags
+git checkout master
+git branch -D $branch
+git push origin :refs/heads/$branch
+
+pushd support
+lein jar
+scp pom.xml cljsbuild-$branch.jar clojars@clojars.org:
+popd
+
+pushd plugin
+lein jar
+scp pom.xml lein-cljsbuild-$branch.jar clojars@clojars.org:
+popd
---
+++ bin/test-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Build the lein-cljsbuild plugin and support JARs, install them
+# locally, and test them in the simple and advanced example projects.
+# If arguments are specified, only build the example projects they name.
+
+set -e
+
+projects="$@"
+[ -z "$projects" ] && projects='simple advanced'
+project_root=$(dirname $(realpath $0))
+pushd $project_root
+
+rm -rf ~/.m2/repository/lein-cljsbuild
+rm -rf ~/.m2/repository/cljsbuild/
+for d in support plugin; do
+	pushd $d
+	lein install
+	popd
+done
+for d in $projects; do
+	pushd example-projects/$d
+	rm -rf .lein-*
+	if [ $d = advanced ]; then
+		extra_command=', cljsbuild test'
+	fi
+	lein deps, cljsbuild clean, cljsbuild once$extra_command
+	popd
+done
