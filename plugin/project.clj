54bda02ee9bea496d4cb7ab2f66b799da9c38721
Evan Mezeske <evan@mezeske.com>
2012-04-16 20:52:39 -0700

    Add an explict maven-artifact dependency.
    Issue #80.

--- plugin/project.clj
+++ plugin/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[fs "1.1.2"]]
+  :dependencies [[fs "1.1.2"]
+                 [org.apache.maven.artifact/maven-artifact "3.0-alpha-1"]]
   :dev-dependencies [[midje "1.3.1"]
                      ; NOTE: lein-midje requires different versions to be
                      ; installed for lein1 vs lein2 compatibility :(.
