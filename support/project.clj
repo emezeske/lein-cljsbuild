d3d4f0afb0eb2de72a6fdf6d89df2462ac1f42df
Evan Mezeske <evan@mezeske.com>
2012-05-09 20:03:56 -0700

    Use clojurescript 0.0-1211.  Closes #88.

--- support/project.clj
+++ support/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/clojurescript "0.0-1011"
+                 [org.clojure/clojurescript "0.0-1211"
                    :exclusions [org.apache.ant/ant]]
                  [fs "1.1.2"]
                  [clj-stacktrace "0.2.4"]]
