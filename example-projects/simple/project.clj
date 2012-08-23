b15d7122ef525934ddf092f54a00308394d567bc
Evan Mezeske <evan@mezeske.com>
2012-08-22 23:13:22 -0700

    Make the examples work with both lein1 and lein2.

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -1,6 +1,9 @@
 (defproject cljsbuild-example-simple "0.2.6"
   :description "A simple example of how to use lein-cljsbuild"
+  ; Source path for Leiningen 1.x:
   :source-path "src-clj"
+  ; Source paths for Leiningen 2.x:
+  :source-paths ["src-clj"]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.0.4"]
                  [hiccup "1.0.0"]]
