dea3d33de35210070d2c2b881d861aeb0014cdbe
Dirk Geurs <dirklectisch@mac.com>
2012-11-24 22:36:29 +0100

    Fixed compatibility issue with Lein 2.x when running the examples.

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -1,17 +1,18 @@
 (defproject cljsbuild-example-simple "0.2.9"
   :description "A simple example of how to use lein-cljsbuild"
-  ; Source path for Leiningen 1.x:
-  :source-path "src-clj"
-  ; Source paths for Leiningen 2.x:
   :source-paths ["src-clj"]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.0.4"]
                  [hiccup "1.0.0"]]
-  :dev-dependencies [[lein-ring "0.7.0"]]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.2.9"]
+            [lein-ring "0.7.0"]]
   :cljsbuild {
     :builds [{:source-path "src-cljs"
               :compiler {:output-to "resources/public/js/main.js"
                          :optimizations :whitespace
                          :pretty-print true}}]}
-  :ring {:handler example.routes/app})
+  :ring {:handler example.routes/app}
+  ; for Leiningen 1.x:
+  :source-path "src-clj"
+  :dev-dependencies [[lein-ring "0.7.0"]]
+  )
