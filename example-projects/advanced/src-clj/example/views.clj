978b96e4676bc8b2f696b603d513ab0d1baa14f9
Evan Mezeske <evan@mezeske.com>
2013-01-20 16:59:07 -0700

    Remove CLOSURE_NO_DEPS.  Closes #135.

--- example-projects/advanced/src-clj/example/views.clj
+++ example-projects/advanced/src-clj/example/views.clj
@@ -6,16 +6,8 @@
       [element :refer [javascript-tag]]
       [page :refer [include-js]]]))
 
-; When using {:optimizations :whitespace}, the Google Closure compiler combines
-; its JavaScript inputs into a single file, which obviates the need for a "deps.js"
-; file for dependencies.  However, true to ":whitespace", the compiler does not remove
-; the code that tries to fetch the (nonexistent) "deps.js" file.  Thus, we have to turn
-; off that feature here by setting CLOSURE_NO_DEPS.
-;
-; Note that this would not be necessary for :simple or :advanced optimizations.
 (defn- run-clojurescript [path init]
   (list
-    (javascript-tag "var CLOSURE_NO_DEPS = true;")
     (include-js path)
     (javascript-tag init)))
 
--- example-projects/simple/src-clj/example/views.clj
+++ example-projects/simple/src-clj/example/views.clj
@@ -2,25 +2,12 @@
   (:require
     [hiccup
       [page :refer [html5]]
-      [element :refer [javascript-tag]]
       [page :refer [include-js]]]))
-
-; When using {:optimizations :whitespace}, the Google Closure compiler combines
-; its JavaScript inputs into a single file, which obviates the need for a "deps.js"
-; file for dependencies.  However, true to ":whitespace", the compiler does not remove
-; the code that tries to fetch the (nonexistent) "deps.js" file.  Thus, we have to turn
-; off that feature here by setting CLOSURE_NO_DEPS.
-;
-; Note that this would not be necessary for :simple or :advanced optimizations.
-(defn- include-clojurescript [path]
-  (list
-    (javascript-tag "var CLOSURE_NO_DEPS = true;")
-    (include-js path)))
 
 (defn index-page []
   (html5
     [:head
       [:title "Hello World"]
-      (include-clojurescript "/js/main.js")]
+      (include-js "/js/main.js")]
     [:body
       [:h1 "Hello World"]]))
