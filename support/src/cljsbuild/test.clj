9e78a5fd839ab32e0b815e6850a2614afc93d6db
Harri Salokorpi <hsalokor@iki.fi>
2013-01-30 20:30:38 +0200

    Throw TestsFailedException when tests fail

--- support/src/cljsbuild/test.clj
+++ support/src/cljsbuild/test.clj
@@ -15,4 +15,4 @@
                   (dofor [test-command test-commands]
                     (util/sh test-command)))]
     (when (not success)
-      (throw (Exception. "Test failed.")))))
+      (throw (cljsbuild.test.TestsFailedException. "Test failed.")))))
