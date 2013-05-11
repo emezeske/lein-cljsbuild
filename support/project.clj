82541811e034c24600af865b008f9dccbc97d5e8
Evan Mezeske <evan@mezeske.com>
2013-05-11 16:56:31 -0700

    Remove a workaround for CLJS-418.  Closes #164.

--- support/project.clj
+++ support/project.clj
@@ -9,8 +9,6 @@
     [[org.clojure/clojure "1.5.1"]
      [org.clojure/clojurescript "0.0-1803"
        :exclusions [org.apache.ant/ant]]
-     ; Ugly workaround for http://dev.clojure.org/jira/browse/CLJS-418
-     [org.clojure/google-closure-library-third-party "0.0-2029-2"]
      [fs "1.1.2"]
      [clj-stacktrace "0.2.5"]]
   :aot [cljsbuild.test]
