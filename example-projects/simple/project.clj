001394a03a4356f43dbd646d8ba2e0e2dbf7f9d2
magomimmo <mimmo.cosenza@gmail.com>
2013-11-07 10:21:13 +0100

    update leingingen :source-paths and update lein-ring to 0.8.8

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -1,13 +1,13 @@
 (defproject cljsbuild-example-simple "1.0.0-alpha1"
   :description "A simple example of how to use lein-cljsbuild"
-  :source-paths ["src-clj"]
+  :source-paths ["src-clj" "src-cljs"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1859"
                   :exclusions [org.apache.ant/ant]]
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]]
   :plugins [[lein-cljsbuild "1.0.0-alpha1"]
-            [lein-ring "0.8.7"]]
+            [lein-ring "0.8.8"]]
   :cljsbuild {
     :builds [{:source-paths ["src-cljs"]
               :compiler {:output-to "resources/public/js/main.js"
