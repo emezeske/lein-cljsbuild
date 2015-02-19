1246da1f407f5f7870a70a5620fafe8238da4ddd
Chas Emerick <chas@cemerick.com>
2015-02-19 16:53:26 -0500

    1.0.6-SNAPSHOT

--- example-projects/none/project.clj
+++ example-projects/none/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2411"]]
 
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :plugins [[lein-cljsbuild "1.0.6-SNAPSHOT"]]
 
   :source-paths ["src"]
 
