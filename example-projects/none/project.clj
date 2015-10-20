b8e3879478d88cb1ef10edca1acb8cd6d168f8c8
Juho Teperi <juho.teperi@iki.fi>
2015-10-20 15:11:22 +0300

    Update clojurescript dependencies

--- example-projects/none/project.clj
+++ example-projects/none/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2411"]]
+                 [org.clojure/clojurescript "0.0-3211"]]
 
   :plugins [[lein-cljsbuild "1.1.0"]]
 
--- support/project.clj
+++ support/project.clj
@@ -7,7 +7,7 @@
      :distribution :repo}
   :dependencies
     [[org.clojure/clojure "1.5.1"]
-     [org.clojure/clojurescript "0.0-2411"
+     [org.clojure/clojurescript "0.0-3211"
        :exclusions [org.apache.ant/ant]]
      [fs "1.1.2"]
      [clj-stacktrace "0.2.5"]]
