20e68a49af39e62b45d7d183f68b1ecb8fb22972
Maria Geller <maria.geller2@gmail.com>
2015-11-01 22:03:18 +1300

    1.1.1-SNAPSHOT

--- doc/CROSSOVERS.md
+++ doc/CROSSOVERS.md
@@ -43,7 +43,7 @@ And your `project.clj` file looks like this:
 
 ```clj
 (defproject lein-cljsbuild-example "1.2.3"
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.1-SNAPSHOT"]]
   :source-paths ["src-clj"]
   :cljsbuild {
     ; Each entry in the :crossovers vector describes a Clojure namespace
--- plugin/project.clj
+++ plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cljsbuild "1.1.0"
+(defproject lein-cljsbuild "1.1.1-SNAPSHOT"
   :description "ClojureScript Autobuilder Plugin"
   :url "http://github.com/emezeske/lein-cljsbuild"
   :license
@@ -12,6 +12,6 @@
     :dev {
       :dependencies [
         [midje "1.6.3"]
-        [cljsbuild "1.1.0"]]
+        [cljsbuild "1.1.1-SNAPSHOT"]]
       :plugins [[lein-midje "3.1.3"]]}}
   :eval-in-leiningen true)
