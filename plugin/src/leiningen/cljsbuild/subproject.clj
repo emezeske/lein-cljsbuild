emezeske/lein-cljsbuild

commit 52587637cabba42ad17754acfeb71f3ea488a169

Use full project config to create subproject.

plugin/src/leiningen/cljsbuild/subproject.clj
@@ -73,13 +73,7 @@
 (defn make-subproject [project crossover-path builds]
   (with-meta
     (merge
-      (select-keys project [:checkout-deps-shares
-                            :eval-in
-                            :jvm-opts
-                            :local-repo
-                            :repositories
-                            :mirrors
-                            :resource-paths])
+      project
       {:local-repo-classpath true
        :dependencies (merge-dependencies (:dependencies project))
        :source-paths (concat

plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-cljsbuild "1.1.3"
+(defproject lein-cljsbuild "1.1.4-SNAPSHOT"
   :description "ClojureScript Autobuilder Plugin"
   :url "http://github.com/emezeske/lein-cljsbuild"
   :license
@@ -10,6 +10,6 @@
     :dev {
       :dependencies [
         [midje "1.6.3"]
-        [cljsbuild "1.1.3"]]
+        [cljsbuild "1.1.4-SNAPSHOT"]]
       :plugins [[lein-midje "3.1.3"]]}}
   :eval-in-leiningen true)

support/project.clj
@@ -1,4 +1,4 @@
-(defproject cljsbuild "1.1.3"
+(defproject cljsbuild "1.1.4-SNAPSHOT"
   :description "ClojureScript Autobuilder"
   :url "http://github.com/emezeske/lein-cljsbuild"
   :license

commit 57a419a8a2f15e46734c725c55692ea5bb82c3f5

Fixes #451.
Build regression has been caused by previous
commit (52587637cabba42ad17754acfeb71f3ea488a169). When creating the
subprobject we either need to dissoc `:root` and `:prep-tasks` or just
select the keys we think we need from the project.

plugin/src/leiningen/cljsbuild/subproject.clj
@@ -84,14 +84,18 @@
 (defn make-subproject [project crossover-path builds]
   (let [deps (get-deps-from-project project)]
     (with-meta
-     (merge
-      project
-      {:local-repo-classpath true
-       :dependencies (merge-dependencies deps)
-       :source-paths (concat
-                      (:source-paths project)
-                      (mapcat :source-paths builds)
-                      [crossover-path])})
-     (meta project))))
-
-
+      (merge
+       (select-keys project [:checkout-deps-shares
+                             :eval-in
+                             :jvm-opts
+                             :local-repo
+                             :repositories
+                             :mirrors
+                             :resource-paths])
+       {:local-repo-classpath true
+        :dependencies (merge-dependencies deps)
+        :source-paths (concat
+                       (:source-paths project)
+                       (mapcat :source-paths builds)
+                       [crossover-path])})
+      (meta project))))