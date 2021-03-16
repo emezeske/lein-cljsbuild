30d8bed5a7ac65dac4494bb9f0668f5f58a814d8
Maria Geller <maria.geller2@gmail.com>
2021-03-16 15:37:47 +0100

    Update simple example project structure

--- example-projects/simple/project.clj
+++ example-projects/simple/project.clj
@@ -1,6 +1,6 @@
 (defproject cljsbuild-example-simple "2.0.0-SNAPSHOT"
   :description "A simple example of how to use lein-cljsbuild"
-  :source-paths ["src-clj"]
+  :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.597"
                   :exclusions [org.apache.ant/ant]]
@@ -9,7 +9,7 @@
   :plugins [[lein-cljsbuild "2.0.0-SNAPSHOT"]
             [lein-ring "0.12.5"]]
   :cljsbuild {
-    :builds [{:source-paths ["src-cljs"]
+    :builds [{:source-paths ["src/cljs"]
               :compiler {:output-to "resources/public/js/main.js"
                          :optimizations :whitespace
                          :pretty-print true}}]}
